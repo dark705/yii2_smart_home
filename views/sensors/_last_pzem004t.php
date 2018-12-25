7fc9500839902c8750392488c8fde69e16f530e4
dark705 <dark705r@mail.ru>
2018-12-25 03:34:03 +0300

    updated js scripts for auto redraw graphs by new data

--- views/sensors/_last_pzem004t.php
+++ views/sensors/_last_pzem004t.php
@@ -25,13 +25,24 @@ $this->registerJs(
     function updateLastElectro(){
         $.getJSON('sensors/json?sensor=pzem004t&last', function(data){
             data = data[0];
+            
+            //update last section
             var d = new Date((data.datetime - 3*60*60) * 1000);
-            data.datetime = d.toString('yyyy-MM-dd HH:mm:ss');
-            $('#last__electro__time span').text(data.datetime);
+            $('#last__electro__time span').text(d.toString('yyyy-MM-dd HH:mm:ss'));
             $('#last__electro__voltage span').text(data.voltage);
             $('#last__electro__current span').text(data.current);
             $('#last__electro__active span').text(data.active);
             $('#last__electro').animate({opacity: 0.1}, 500).animate({opacity: 1.0}, 500)
+            
+            //update graph
+			var chartPzem004t = $('#pzem004t').highcharts();
+			var voltage = [data.datetime * 1000, data.voltage];
+			var current = [data.datetime * 1000, data.current];
+			var active = [data.datetime * 1000, data.active];		
+			chartPzem004t.series[0].addPoint(voltage, false, true);
+			chartPzem004t.series[1].addPoint(current, false, true);
+			chartPzem004t.series[2].addPoint(active, false, true);
+			chartPzem004t.redraw();   
         });
     }
     setInterval(function(){
