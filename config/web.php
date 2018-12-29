d0d63370b2024b8ede9ffd23e127e5e6eac9966b
dark705 <dark705r@mail.ru>
2018-12-29 15:15:48 +0300

    make config depend from YII_DEBUG const setting

--- config/web.php
+++ config/web.php
@@ -5,19 +5,12 @@
  * Date: 10.12.2018
  * Time: 16:18
  */
-return [
+$config = [
     'id' => 'manage',
     'basePath' => '../',
-    'bootstrap' => ['debug'],
 	'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
-    ],
-    'modules' => [
-        'debug' => [
-            'class' => 'yii\debug\Module',
-            'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*']
-        ]
     ],
     'components' => [
         'request' => [
@@ -33,3 +26,12 @@ return [
         ]
     ]
 ];
+
+if (YII_ENV_DEV) {
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*'],
+    ];
+}
+return $config;
--- web/index.php
+++ web/index.php
@@ -5,9 +5,13 @@
  * Date: 10.12.2018
  * Time: 15:57
  */
-define ('YII_DEBUG', true);
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
 require '../vendor/autoload.php';
 require '../vendor/yiisoft/yii2/Yii.php';
+
 $config = require '../config/web.php';
 (new yii\web\Application($config))->run();
 
